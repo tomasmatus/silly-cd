6a6fc48355bce6bf7c1459d6e067a7320aeb6e5b
tomasmatus <tomatus777@tomatus.cz>
2025-11-14 15:44:04 +0100

    podmanCD: add systemd calls

--- src/podman_cd.py
+++ src/podman_cd.py
@@ -4,6 +4,7 @@ import subprocess
 
 from gitforge import DirChangeStatus, FileStatus, GitForge
 from podman import Podman
+from systemctl import Systemctl
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +13,7 @@ class PodmanCD:
         self.work_dir = work_dir
         self.forge = GitForge(work_dir)
         self.podman = Podman()
+        self.systemctl = Systemctl(user_mode=True)
 
     def check_for_updates(self):
         logger.info("Checking for updates...")
@@ -55,13 +57,14 @@ class PodmanCD:
         for image in images:
             try:
                 logger.info(f"Pulling image: {image}")
-                self.podman.podman_pull(image)
+                self.podman.pull(image)
                 logger.info(f"Done pulling: {image}")
             except subprocess.CalledProcessError as e:
                 logger.error(f"Failed to pull image {image}")
                 raise e
 
     def handle_services_lifecycle(self , changed_dirs: list[DirChangeStatus]):
+        self.systemctl.daemon_reload()
         for dir_change in changed_dirs:
             kube_service = self.get_files_endswith(dir_change.dir_name, (".kube"))
             if len(kube_service) == 0:
@@ -71,11 +74,11 @@ class PodmanCD:
 
             if dir_change.status in [FileStatus.ADDED, FileStatus.MODIFIED]:
                 logger.info(f"Restarting kube {kube_service}")
-                # systemctl --user restart kube_service
+                # restart the service, if it is not running it will be started
+                self.systemctl.restart(kube_service)
 
             elif dir_change.status == FileStatus.DELETED:
-                pass
-                # systemctl --user stop kube_service
+                self.systemctl.stop(kube_service)
 
             else:
                 logger.warning(f"Unsupported file status: {dir_change.status} for {dir_change.dir_name}, not doing anything!")
