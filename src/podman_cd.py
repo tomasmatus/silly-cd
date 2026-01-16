b787ae27082395e062f5df1dbdf93f8f36163320
tomasmatus <tomatus777@tomatus.cz>
2026-01-16 17:08:21 +0100

    reword podman_cd to use new ChangedDir structure format
    should fix deleting a service

--- src/podman_cd.py
+++ src/podman_cd.py
@@ -2,7 +2,7 @@ import logging
 import os
 import subprocess
 
-from gitforge import DirChangeStatus, FileStatus, GitForge
+from gitforge import ChangedDir, ChangedFile, DirStatus, FileMap, FileStatus, GitForge
 from podman import Podman
 from systemctl import Systemctl
 
@@ -30,33 +30,49 @@ class PodmanCD:
         self.handle_services_lifecycle(changed_dirs)
 
     @staticmethod
-    def parse_yaml_for_images(file_name: str) -> list[str]:
-        images: list[str] = []
+    def parse_yaml_for_images(file_name: str) -> set[str]:
+        images: set[str] = set()
 
         with open(file_name, "r") as f:
             for line in f:
                 line = line.strip()
                 if line.startswith("image:"):
                     image = line.split("image:")[1].strip()
-                    images.append(image)
+                    images.add(image)
 
         return images
 
     def get_files_endswith(self, dir_name: str, suffix: str | tuple[str, ...]) -> list[str]:
         full_path = f"{self.work_dir}/{dir_name}"
         dir_content = os.listdir(full_path)
-        return [f"{full_path}/{f}" for f in dir_content if f.endswith(suffix)]
+        return [f for f in dir_content if f.endswith(suffix)]
 
-    def fetch_new_images(self, changed_dirs: set[DirChangeStatus]):
-        images: list[str] = []
-        for dir_change in changed_dirs:
-            if dir_change.status not in [FileStatus.ADDED, FileStatus.MODIFIED]:
-                continue
+    def get_service_name(self, dir: ChangedDir) -> str | None:
+        def kube_to_service(name):
+            return name.replace(".kube", ".service")
 
-            yamls = self.get_files_endswith(dir_change.dir_name, (".yml", ".yaml"))
+        # try to find it in FileStatus list
+        for file in dir.files:
+            if file.filename.endswith(".kube"):
+                return kube_to_service(file.filename)
 
-            for yaml in yamls:
-                images.extend(self.parse_yaml_for_images(yaml))
+        if dir.dir_status == DirStatus.MODIFIED:
+            # try to find it in the directory
+            try:
+                kube_files = self.get_files_endswith(dir.dirname, ".kube")
+                if len(kube_files) > 0:
+                    return kube_to_service(kube_files[0])
+            except FileNotFoundError as e:
+                logger.error(f"Unable to find kube file in directory '{dir.dirname}:\n{e}")
+
+        return None
+
+    def fetch_new_images(self, changed_dirs: FileMap):
+        images: set[str] = set()
+        for dir in changed_dirs.values():
+            for file in dir.files:
+                if file.filename.endswith((".yaml", ".yml")) and file.status in [FileStatus.ADDED, FileStatus.MODIFIED]:
+                    images.union(self.parse_yaml_for_images(f"{dir.dirname}/{file.filename}"))
 
         for image in images:
             try:
@@ -67,22 +83,34 @@ class PodmanCD:
                 logger.error(f"Failed to pull image {image}")
                 raise e
 
-    def handle_services_lifecycle(self, changed_dirs: set[DirChangeStatus]):
+    def modified_service(self, dir: ChangedDir):
+        kube_service = self.get_service_name(dir)
+        if kube_service == None:
+            logger.info(f"Skipping {dir.dirname}")
+            return
+
+        logger.info(f"Restarting kube {kube_service}")
+        # restart the service, if it is not running it will be started
+        self.systemctl.restart(kube_service)
+
+    def deleted_service(self, dir: ChangedDir):
+        kube_service = self.get_service_name(dir)
+        if kube_service == None:
+            logger.info(f"Skipping {dir.dirname}")
+            return
+
+        logger.info(f"Stopping deleted kube {kube_service}")
+        self.systemctl.stop(kube_service)
+
+    def handle_services_lifecycle(self, changed_dirs: FileMap):
         self.systemctl.daemon_reload()
-        for dir_change in changed_dirs:
-            kube_service = self.get_files_endswith(dir_change.dir_name, (".kube"))
-            if len(kube_service) == 0:
-                continue
-            else:
-                kube_service = os.path.basename(kube_service[0].replace(".kube", ".service"))
+        for dir in changed_dirs.values():
+            if dir.dir_status == DirStatus.MODIFIED:
+                self.modified_service(dir)
 
-            if dir_change.status in [FileStatus.ADDED, FileStatus.MODIFIED]:
-                logger.info(f"Restarting kube {kube_service}")
-                # restart the service, if it is not running it will be started
-                self.systemctl.restart(kube_service)
-
-            elif dir_change.status == FileStatus.DELETED:
-                self.systemctl.stop(kube_service)
+            elif dir.dir_status == DirStatus.DELETED:
+                self.deleted_service(dir)
 
             else:
-                logger.warning(f"Unsupported file status: {dir_change.status} for {dir_change.dir_name}, not doing anything!")
+                logger.warning(f"Unsupported dir status: {dir.dir_status} for {dir.dirname}, not doing anything!")
+
