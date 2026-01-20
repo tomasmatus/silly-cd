403f2d292719f6622c68638c9db485be3c7c9a74
tomasmatus <tomatus777@tomatus.cz>
2026-01-20 15:31:11 +0100

    reword to use Difftool

--- src/gitforge.py
+++ src/gitforge.py
@@ -1,36 +1,18 @@
 import logging
 import os
 import subprocess
-from dataclasses import dataclass
-from enum import Enum
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-class FileStatus(Enum):
-    ADDED = "A"
-    MODIFIED = "M"
-    DELETED = "D"
-    RENAMED = "R"
-    COPIED = "C"
-    UNCHANGED = " "
-
-@dataclass
-class DirChangeStatus:
-    dir_name: str
-    status: FileStatus
-
-    def __hash__(self):
-        return hash(self.dir_name)
-
-
 class GitForge:
-    def __init__(self, work_dir: str):
-        if not os.path.isdir(work_dir):
-            raise FileNotFoundError(f"Directory does not exist: {work_dir}")
-        if not os.path.exists(os.path.join(work_dir, '.git')):
+    def __init__(self, work_dir: Path):
+        if not work_dir.is_dir():
+            raise NotADirectoryError(f"Not a directory: {work_dir}")
+        if not os.path.exists(work_dir / ".git"):
             raise FileNotFoundError(f"Not a git repository: {work_dir}")
 
-        self.work_dir = work_dir
+        self.work_dir = work_dir.resolve()
 
     def run_git_command(self, *args) -> str:
         """
@@ -70,33 +52,3 @@ class GitForge:
 
     def latest_commit_hash(self) -> str:
         return self.run_git_command("rev-parse", "HEAD")
-
-    def find_changed_dirs(self, commit1: str, commit2: str = "HEAD") -> set[DirChangeStatus]:
-        diff_changed = self.git_diff_files_range(commit1, commit2)
-
-        changed_dirs: set[DirChangeStatus] = set()
-        if not diff_changed:
-            return changed_dirs
-
-        for line in diff_changed.splitlines():
-            if not line.strip():
-                continue
-
-            parts = line.split("\t", 1)
-            if len(parts) != 2:
-                logger.warning(f"Unexpected diff output format: '{line}'... ignoring")
-                continue
-
-            status_code = parts[0].strip()
-            dir_name = os.path.dirname(parts[1].strip())
-
-            if dir_name == "":
-                continue
-
-            try:
-                status = FileStatus(status_code)
-                changed_dirs.add(DirChangeStatus(dir_name=dir_name, status=status))
-            except ValueError:
-                raise ValueError(f"Unknown dir status: {status_code} for file {dir_name}")
-
-        return changed_dirs
