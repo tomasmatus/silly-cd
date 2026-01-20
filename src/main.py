3c89dcea42fac227f359af0e7b06cfe1351269d2
tomasmatus <tomatus777@tomatus.cz>
2026-01-20 13:01:14 +0100

    add difftool to find directory changes

--- src/main.py
+++ src/main.py
@@ -2,19 +2,25 @@
 import argparse
 import logging
 
-from gitforge import GitForge
 from podman_cd import PodmanCD
+from difftool import DiffTool
 
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
     parser = argparse.ArgumentParser(description='Silly CD')
-    parser.add_argument('--cwd', type=str, help='Root directory of monitored repository', default=".")
+    parser.add_argument('--repo-dir', type=str, help='Root directory of monitored repository', default=".")
+    parser.add_argument('--deploy-dir', type=str, help='Root directory of deployed directory', default=".")
 
     args = parser.parse_args()
 
-    podmanCD = PodmanCD(args.cwd, True)
-    podmanCD.run_update()
+    diffs = DiffTool(args.repo_dir, args.deploy_dir).list_changed_deployments()
+
+    for diff in diffs:
+        print(f"{diff.dirname}: {diff.status}")
+
+    # podmanCD = PodmanCD(args.cwd, True)
+    # podmanCD.run_update()
 
 if __name__ == "__main__":
     main()
